b8ebc6146553785646968686fe322014f94f359f
Daniel_Guerrero <dag83@njit.edu>
2025-05-30 03:05:58 -0400

    Program that takes in 2 2-dimensional arrays as matrices and multiplies them. Has functions for getting matrix column, multiplying the row/column, multiplying the matrix all together, and printing the results

--- first-kotlin-proj/src/main/kotlin/Main.kt
+++ first-kotlin-proj/src/main/kotlin/Main.kt
@@ -1,14 +1,44 @@
 package org.example
 
 fun main() {
-    var x=5
+    var name:String?=null
+    var w0:Int=4;var w1:Int=11
+    val x0:Float=5f;val x1:Float=2.2f
+    val y0:Boolean=true
+    println(w1%w0)
+    println(y0)
+    w0=rect(w0,w1)
+    when(w0%3)
+    {
+        0->println("divisible by 3")
+        1->println("1 off from 3 multiple")
+        2->println("2 off from 3 multiple")
+    }
+    /*var x=5
     var y=3
     var z=x+y
     println("${x}+${y}=${z}")
     println("Hello World!")
     println("${x}!=${factorial(x)}")
     println("${y}!=${factorial(y)}")
-    println("${z}!=${factorial(z)}")
+    println("${z}!=${factorial(z)}")*/
+}
+fun rect(a:Int, b:Int):Int
+{
+    if(a<0||b<0)
+    {
+        return 0;
+    }
+    for(i in 1..a)
+    {
+        print('\n')
+        for(j in 1..b)
+        {
+            print('*')
+        }
+    }
+    println('\n')
+    return a*b
 }
 fun factorial(a:Int): Int
 {
---
+++ kot-matrix-mult/src/main/kotlin/Main.kt
@@ -0,0 +1,121 @@
+package org.example
+import java.util.Scanner
+import java.util.Random
+fun main() {
+    val X1:Int=2; val X2:Int=2;val Y1:Int=2;val Y2:Int=2
+    //random "seed"
+    val random=Random()
+    /*try {
+        println("Enter X axis of Matrix 1:")
+        X1 = Scanner(System.`in`).nextInt()
+        println("Enter Y axis of Matrix 1:")
+        Y1 = Scanner(System.`in`).nextInt()
+        println("Enter X axis of Matrix 2:")
+        X2 = Scanner(System.`in`).nextInt()
+        println("Enter Y axis of Matrix 2:")
+        Y2 = Scanner(System.`in`).nextInt()
+        if(X1!=Y2)
+        {
+            throw IllegalArgumentException("Can only multiply Matrices with same x axis and y axis of matrices 1 and 2 respectively")
+        }
+    }
+    catch (e:IllegalArgumentException){
+        println("Invalid axes: ${e.message}")
+    }*/
+
+    //declare matrix arrays
+    val m1=Array(Y1){Array<Int>(X1){0}}
+    val m2=Array(Y2){Array<Int>(X2){0}}
+    //print out initial matrices
+    println("Matrix 1:")
+    for(i in 0..Y1-1)
+    {
+        for (j in 0..X1-1)
+        {
+            m1[i][j]=random.nextInt(21)-10
+        }
+    }
+    printMatrix(m1)
+    println("\nMatrix 2:")
+    for(i in 0..Y2-1)
+    {
+        for (j in 0..X2-1)
+        {
+            m2[i][j]=random.nextInt(21)-10
+        }
+    }
+    printMatrix(m2)
+println("")
+    //Determine if |M1[0]|==|M2|
+    try {
+        if(X1!=Y2)
+        {
+            throw IllegalArgumentException("\nCan only multiply Matrices with same x axis and y axis of matrices 1 and 2 respectively")
+        }
+    }
+    catch (e:IllegalArgumentException){
+    println("\nInvalid axes: ${e.message}")
+    }
+
+    var col= getColumn(m2)
+    for (i in col)
+    {
+        println(i)
+    }
+    val oo= multStep(col,m1[0])
+    println(oo)
+
+    var qanon= multMatrix(m1,m2)
+    printMatrix(qanon)
+}
+
+//make an array based on the column
+fun getColumn(m:Array<Array<Int>>,index:Int=0):Array<Int>
+{
+    var retu=Array(m.size){0}
+    for(i in 0..m.size-1)
+    {
+        retu[i]=m[i][index]
+    }
+    return retu
+}
+
+//multiplication of row/column step
+fun multStep(m:Array<Int>,n:Array<Int>):Int
+{
+    var prod=0
+    for(i in 0..m.size-1) {
+        prod += m[i] * n[i]
+    }
+    return prod
+}
+
+//multiply matrix algorithm
+fun multMatrix(m:Array<Array<Int>>,n:Array<Array<Int>>):Array<Array<Int>>
+{
+    var reto=Array(m.size){Array<Int>(n[0].size){0}}
+    val mark=m.size-1
+    var s:Int=0
+    for(i in 0..mark)
+    {
+        for(j in 0..n[0].size-1)
+        {
+            s=multStep(m[i], getColumn(n,j))
+            reto[i][j]=s
+        }
+    }
+    return reto
+}
+
+//reusable printer for matrices
+fun printMatrix(m:Array<Array<Int>>):Unit
+{
+    for(i in 0..m.size-1)
+    {
+        for(j in 0..m[0].size-1)
+        {
+            print("${m[i][j].toString().padStart(4,' ')}\t")
+        }
+        print('\n')
+    }
+}
